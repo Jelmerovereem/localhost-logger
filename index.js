a546b270ae091170b1dec50f6906a8093392b1c7
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-03-30 13:15:15 +0200

    updated

---
+++ index.js
@@ -0,0 +1,21 @@
+const networkInterfaces = require("os");
+
+const localLog = (port) => {
+	const nets = networkInterfaces();
+	const results = Object.create({});
+	for (const name of Object.keys(nets)) {
+		for (const net of nets[name]) {
+			if (net.family === "IPv4" && !net.internal) {
+				if (!results[name]) {
+					results[name] = [];
+				}
+				results[name].push(net.address);
+			}
+		}
+	}
+	let ipstring;
+	results.wifi0 ? ipstring = `${results.wifi0[0]}:${port}` : ipstring = "...IPv4 not available...";
+	return ipstring;
+}
+
+module.exports = localLog;
